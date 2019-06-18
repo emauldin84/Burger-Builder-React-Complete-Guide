3aa0adca7c3c8b1e8633649611acc0aff5e49af5
Eric Mauldin <emauldin84@gmail.com>
2019-06-18 16:58:17 -0400

    shows modal when Order Now button is clicked using inline styling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,6 +24,7 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
+        purchasing: false,
     }
 
     updatePurchaseState (ingredients) {
@@ -73,6 +74,12 @@ class BurgerBuilder extends Component {
 
     }
 
+    purchaseHandler = () => {
+        this.setState({
+            purchasing: true,
+        })
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -84,7 +91,7 @@ class BurgerBuilder extends Component {
         }
         return(
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
@@ -93,6 +100,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice}
                     />
             </Aux>
