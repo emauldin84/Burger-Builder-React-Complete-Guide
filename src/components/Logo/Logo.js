7ffc162320757395c9daaabf64219e1b345293e4
Eric Mauldin <emauldin84@gmail.com>
2019-06-20 10:44:23 -0400

    adds logo styling for mobile and larger screens

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,8 +2,8 @@ import React from 'react'
 import burgerLogo from '../../assets/images/burger-logo.png'
 import classes from './Logo.css'
 
-const logo = () => (
-    <div className={classes.Logo}>
+const logo = (props) => (
+    <div className={classes.Logo} >
         <img src={burgerLogo} alt="Burger Builder"/>
     </div>
 );
