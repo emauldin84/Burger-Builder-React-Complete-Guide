f76b5862f6329e4536d99b02caefa57dbc556e48
Eric Mauldin <emauldin84@gmail.com>
2019-06-20 13:55:05 -0400

    adds shouldComponentUpdate check to modal to avoid unnecessary updating

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import classes from './Modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal} 
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-150vh)', 
-                opacity: props.show ? '1': '0'
-                }}>
-            {props.children}
-        </div>
+class Modal extends Component {
 
-    </Aux>
-);
+    shouldComponentUpdate(nextProps, nextState) {
+        // no need to update modal and order summary if the modal is not showing
+        return nextProps.show !== this.props.show
+    }
 
-export default modal
+    componentDidUpdate() {
+        console.log('[Modal] DidUpdate')
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                    className={classes.Modal} 
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-150vh)', 
+                        opacity: this.props.show ? '1': '0'
+                        }}>
+                    {this.props.children}
+                </div>
+
+            </Aux>
+
+        )
+    }
+
+}
+
+export default Modal
