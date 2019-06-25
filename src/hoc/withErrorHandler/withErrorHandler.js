0d3b3e7b6f8c8f3db8d51c7b6d2373aaf459f16c
Eric Mauldin <emauldin84@gmail.com>
2019-06-25 15:00:02 -0400

    ejects request and response interceptors when the burgerBuilder component has been unmounted

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,28 +7,26 @@ const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
         constructor(props){
             super(props)
-            axios.interceptors.request.use(req => {
+
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({ error: null })
                 return req
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 console.log('error', error)
                 this.setState({ error: error })
             })
+
         }
         state = {
             error: null
         }
-        // componentDidMount() {
-        //     axios.interceptors.request.use(req => {
-        //         this.setState({ error: null })
-        //         return req
-        //     })
-        //     axios.interceptors.response.use(res => res, error => {
-        //         console.log('error', error)
-        //         this.setState({ error: error })
-        //     })
-        // }
+        
+        componentWillUnmount () {
+            axios.interceptors.request.eject(this.reqInterceptor)
+            axios.interceptors.response.eject(this.resInterceptor)
+        }
+
         errorConfirmedHandler = () => {
             this.setState({error: null})
         }
