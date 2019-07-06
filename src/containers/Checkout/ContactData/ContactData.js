1ea05da8d3c68bab9658a1c467938836441cb998
Eric Mauldin <emauldin84@gmail.com>
2019-07-06 15:26:29 -0400

    adds validation feedback logic and styling

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,9 +84,6 @@ class ContactData extends Component {
                     ]
                 },
                 value: '',
-                validation: {
-                    required: true
-                },
                 valid: false,
             }
         },
@@ -167,6 +164,8 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
                 <Button btnType='Success'>Order</Button>
