5681619087c6de822233a1dc4bdfa8875b98a569
Eric Mauldin <emauldin84@gmail.com>
2019-07-08 09:30:55 -0400

    fixes error with missing validation boolean on the dropdown menu

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -89,6 +89,7 @@ class ContactData extends Component {
                     ]
                 },
                 value: '',
+                validation: {},
                 valid: true,
             }
         },
@@ -126,6 +127,10 @@ class ContactData extends Component {
 
     checkValidity(value, rules) {
         let isValid = true
+        
+        if(!rules) {
+            return true
+        }
         if (rules.required) {
             isValid = value.trim() !== '' && isValid
         }
