e93f0b4389f7cf428764f75c707d54ba697cf952
Eric Mauldin <emauldin84@gmail.com>
2019-07-29 15:43:40 -0400

    updates burgerBuilder purchase button to display sign in to continue if user is not authenticated and redirects the user to login if it is clicked. otherwise it displays order now as before

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,9 +36,13 @@ class BurgerBuilder extends Component {
     
 
     purchaseHandler = () => {
-        this.setState({
-            purchasing: true,
-        })
+        if (this.props.isAuthenticated){
+            this.setState({
+                purchasing: true,
+            })
+        } else {
+            this.props.history.push('/auth')
+        }
     }
 
     purshaseCancelHandler = () => {
@@ -72,6 +76,7 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}
                         price={this.props.price}
                         />
                 </Aux>
@@ -100,6 +105,7 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
         error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null,
     }
 }
 const mapDispatchToProps = dispatch => {
