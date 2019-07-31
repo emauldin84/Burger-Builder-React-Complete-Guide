5247209d7113b7dc49750977db2ff91b0322b587
Eric Mauldin <emauldin84@gmail.com>
2019-07-31 14:27:57 -0400

    utilizes the .contains enzyme method to check for the logout navigationItem

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,4 +23,8 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({isAuthenticated: true})
         expect(wrapper.find(NavigationItem)).toHaveLength(3)
     })
+    it("should contain <NavigationItem link='/logout'>Logout</NavigationItem> elements if authenticated", () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true)
+    })
 })
